ff2ad217fb3f8a2b4ac5e7f89b0fd87822677f96
Calbabreaker <calbabreaker@gmail.com>
2021-10-21 21:02:04 +1030

    added stuff

---
+++ yacpm_test_lib.cpp
@@ -0,0 +1,3 @@
+#include "yacpm_test_lib.h"
+
+glm::vec3 do_math(const glm::vec2 &a, const glm::vec2 &b) {}
---
+++ yacpm_test_lib.h
@@ -0,0 +1,3 @@
+#include <glm/glm.hpp>
+
+glm::vec3 do_math(const glm::vec2 &a, const glm::vec2 &b);
